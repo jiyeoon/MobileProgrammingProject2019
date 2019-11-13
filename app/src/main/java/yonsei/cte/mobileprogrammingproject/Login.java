12db972af9cc6668b6d7f3fb895ab868975a2936
jiyeon <i3048i@naver.com>
2019-11-13 22:02:18 +0900

    Insert fireabase

--- app/src/main/java/yonsei/cte/mobileprogrammingproject/Login.java
+++ app/src/main/java/yonsei/cte/mobileprogrammingproject/Login.java
@@ -1,30 +1,23 @@
 package yonsei.cte.mobileprogrammingproject;
 
-import android.content.Intent;
+import android.app.Activity;
 import android.os.Bundle;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login extends AppCompatActivity {
 
     private static final String TAG = "EmailPassword";
 
@@ -38,50 +31,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_login);
 
         //start Initialize_auth
         //Initialize firebase auth
         mAuth = FirebaseAuth.getInstance();
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onStart() {
+    protected void onStart() {
         super.onStart();
 
         //Check if user is signed in
@@ -89,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         //updateUI(currentUser);
     }
 
-    /*
     private void createAccount(String email, String password){
         //Log.d(TAG, "createAccount : " + email);
         if(!validatedForm()){
@@ -101,21 +58,21 @@ public class MainActivity extends AppCompatActivity {
         //[start create user with eamil]
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    //Sign in success!!!!!
-                    Log.d(TAG, "createUserwithEmail:success");
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    //UpdateUI(user);
-                } else{
-                    //sign in failed.
-                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                    Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
-                    //UpdateUI(null);
-                }
-            }
-        });
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //Sign in success!!!!!
+                            Log.d(TAG, "createUserwithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //UpdateUI(user);
+                        } else{
+                            //sign in failed.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+                            //UpdateUI(null);
+                        }
+                    }
+                });
 
     }
 
@@ -175,10 +132,5 @@ public class MainActivity extends AppCompatActivity {
 
         return valid;
     }
-    */
 
-    public void onClick(View view) {
-        Intent intent = new Intent(getApplicationContext(), Login.class);
-        startActivity(intent);
-    }
 }
