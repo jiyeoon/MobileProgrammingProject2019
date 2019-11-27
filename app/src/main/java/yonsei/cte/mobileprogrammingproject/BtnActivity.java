a148828a14aa68b602a67c6f440d97b48ed00ba6
Jokyuseong <Jokyuseong@users.noreply.github.com>
2019-11-28 01:14:38 +0900

    add-gif

--- app/src/main/java/yonsei/cte/mobileprogrammingproject/BtnActivity.java
+++ app/src/main/java/yonsei/cte/mobileprogrammingproject/BtnActivity.java
@@ -8,12 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+
 import yonsei.cte.mobileprogrammingproject.databinding.ActivityBtnBinding;
 
 public class BtnActivity extends AppCompatActivity {
 
     ActivityBtnBinding binding;
     int btn_condition = 0;
+    int num_contents = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +37,7 @@ public class BtnActivity extends AppCompatActivity {
         binding.btngonextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (btn_condition <= 5)
+                if (btn_condition <= num_contents)
                     btn_condition = btn_condition + 1;
                 setCondition(btn_condition);
             }
@@ -46,49 +50,36 @@ public class BtnActivity extends AppCompatActivity {
                 setCondition(btn_condition);
             }
         });
+        Glide.with(this).load(R.drawable.btn1).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(binding.btn1Image);
+        Glide.with(this).load(R.drawable.btn2).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(binding.btn2Image);
+        Glide.with(this).load(R.drawable.btn3).diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(binding.btn3Image);
     }
 
     private void setCondition(int btn_condition) {
         switch (btn_condition) {
             case 0:
-                binding.btn1Text.setVisibility(binding.btn1Text.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn2Text.setVisibility(binding.btn2Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn1Image.setVisibility(binding.btn1Image.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn2Image.setVisibility(binding.btn2Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btngoprevButton.setVisibility(binding.btngoprevButton.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.INVISIBLE);
+                binding.btn1Text.setVisibility(View.VISIBLE);
+                binding.btn2Text.setVisibility(View.GONE);
+                binding.btn1Image.setVisibility(View.VISIBLE);
+                binding.btn2Image.setVisibility(View.GONE);
+                binding.btngoprevButton.setVisibility(View.INVISIBLE);
                 break;
             case 1:
-                binding.btn1Text.setVisibility(binding.btn1Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn2Text.setVisibility(binding.btn2Text.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn3Text.setVisibility(binding.btn3Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn1Image.setVisibility(binding.btn1Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn2Image.setVisibility(binding.btn2Image.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn3Image.setVisibility(binding.btn3Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btngoprevButton.setVisibility(binding.btngoprevButton.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
+                binding.btn1Text.setVisibility(View.GONE);
+                binding.btn2Text.setVisibility(View.VISIBLE);
+                binding.btn3Text.setVisibility(View.GONE);
+                binding.btn1Image.setVisibility(View.GONE);
+                binding.btn2Image.setVisibility(View.VISIBLE);
+                binding.btn3Image.setVisibility(View.GONE);
+                binding.btngoprevButton.setVisibility(View.VISIBLE);
+                binding.btngonextButton.setVisibility(View.VISIBLE);
                 break;
             case 2:
-                binding.btn2Text.setVisibility(binding.btn2Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn3Text.setVisibility(binding.btn3Text.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn4Text.setVisibility(binding.btn4Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn2Image.setVisibility(binding.btn2Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn3Image.setVisibility(binding.btn3Image.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn4Image.setVisibility(binding.btn4Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                break;
-            case 3:
-                binding.btn3Text.setVisibility(binding.btn3Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn4Text.setVisibility(binding.btn4Text.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn5Text.setVisibility(binding.btn5Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn3Image.setVisibility(binding.btn3Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn4Image.setVisibility(binding.btn4Image.getVisibility() == View.VISIBLE ? View.VISIBLE : View.VISIBLE);
-                binding.btn5Image.setVisibility(binding.btn5Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btngonextButton.setVisibility(binding.btngonextButton.getVisibility() == View.INVISIBLE ? View.VISIBLE : View.VISIBLE);
-                break;
-            case 4:
-                binding.btn4Text.setVisibility(binding.btn4Text.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn5Text.setVisibility(binding.btn5Text.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
-                binding.btn4Image.setVisibility(binding.btn4Image.getVisibility() == View.VISIBLE ? View.GONE : View.GONE);
-                binding.btn5Image.setVisibility(binding.btn5Image.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
-                binding.btngonextButton.setVisibility(binding.btngonextButton.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.INVISIBLE);
+                binding.btn2Text.setVisibility(View.GONE);
+                binding.btn3Text.setVisibility(View.VISIBLE);
+                binding.btn2Image.setVisibility(View.GONE);
+                binding.btn3Image.setVisibility(View.VISIBLE);
+                binding.btngonextButton.setVisibility(View.INVISIBLE);
                 break;
 
         }
